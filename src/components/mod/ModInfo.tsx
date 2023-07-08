6f403035c806bf0c5b1f80ed0cc0a9f2ee6a3c33
Rochedo098 <75433591+Roxxedo@users.noreply.github.com>
2023-07-08 13:54:46 -0300

    Fixed?

--- src/components/mod/ModInfo.tsx
+++ src/components/mod/ModInfo.tsx
@@ -6,13 +6,13 @@ export default function ModInfo({ value }: GenericInputPropsData) {
     return (
         <>
             <h2>{value.name}</h2>
-            <div className='d-flex text-muted pb-4 mb-2 border-bottom border-white'>
+            <div className='d-flex text-white pb-4 mb-2 border-bottom border-white'>
                 <Img style={{display: 'inline-block'}} className="rounded-3 me-2" src={[("https://raw.githubusercontent.com/" + value.repo + "/master/icon.png"), ("https://raw.githubusercontent.com/" + value.repo + "/master/assets/icon.png"), "https://raw.githubusercontent.com/Anuken/MindustryJavaModTemplate/master/assets/sprites/frog.png"]} alt="Mod Icon" width="128" height="128" />
                 <div style={{display: 'inline-block'}}>
-                    <span>Repo: <Link href={'https://github.com/' + value.repo} className='text-muted text-decoration-none'>{value.repo}</Link></span><br/>
-                    <span>Author: <Link href={'https://github.com/' + value.repo.split('/')[0]} className='text-muted text-decoration-none'>{value.author}</Link></span><br/>
+                    <span>Repo: <Link href={'https://github.com/' + value.repo} className='text-white text-decoration-none'>{value.repo}</Link></span><br/>
+                    <span>Author: <Link href={'https://github.com/' + value.repo.split('/')[0]} className='text-white text-decoration-none'>{value.author}</Link></span><br/>
                     <span>Stars: {value.stars}</span><br/>
-                    <span>Game Version: <Link href={'https://github.com/Anuken/Mindustry/releases/tag/v' + value.minGameVersion} className='text-muted text-decoration-none'>{value.minGameVersion}</Link></span><br/>
+                    <span>Game Version: <Link href={'https://github.com/Anuken/Mindustry/releases/tag/v' + value.minGameVersion} className='text-white text-decoration-none'>{value.minGameVersion}</Link></span><br/>
                 </div>
             </div>
         </>
--- src/components/mod/VersionsListElement.tsx
+++ src/components/mod/VersionsListElement.tsx
@@ -3,7 +3,7 @@ import { GenericInputPropsGithub } from "@/lib/types";
 export default function GHListElement({ value }: GenericInputPropsGithub) {
     return (
         <>
-            <div className="list-group-item list-group-item-action my-1 lh-sm text-white rounded-4 shadow" style={{background: "#26292f"}} aria-current="true">
+            <div className="list-group-item list-group-item-action my-1 lh-sm text-white rounded-4 shadow border-0" style={{background: "#26292f"}} aria-current="true">
                 <div className="d-flex w-100 align-items-center justify-content-between">
                     <h2 className="mb-1 font-weight-bold">{value.name} | {value.tag_name}</h2>
                 </div>
@@ -13,7 +13,7 @@ export default function GHListElement({ value }: GenericInputPropsGithub) {
                         <div className="d-flex w-100 align-items-center justify-content-between">
                             <h4 className="mb-1 font-weight-bold">{value.name}</h4>
                         </div>
-                        <div className="mb-1 text-muted"> <span>Download count: {value.download_count}</span> </div>
+                        <div className="mb-1 text-white"> <span>Download count: {value.download_count}</span> </div>
                         <a className="mb-1 btn btn-primary " href={value.browser_download_url}>Download</a>
                     </>
                 ))}
