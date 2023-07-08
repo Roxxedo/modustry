cf556f957f7ee125ee5a22bab0200e5614376520
Rochedo098 <75433591+Roxxedo@users.noreply.github.com>
2023-07-08 13:33:52 -0300

    New logo and some bug fixes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,29 +15,29 @@ export default function Footer() {
                     <div className="col mb-3">
                       <h5>Content</h5>
                       <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><a href={process.env.API_URL + ""} className="nav-link p-0 text-body-secondary">Home</a></li>
-                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/mods"} className="nav-link p-0 text-body-secondary">Mods</a></li>
-                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/modpacks"} className="nav-link p-0 text-body-secondary">Modpacks</a></li>
-                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/textures"} className="nav-link p-0 text-body-secondary">Textures</a></li>
+                        <li className="nav-item mb-2"><a href={process.env.API_URL + ""} className="nav-link p-0 text-white">Home</a></li>
+                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/mods"} className="nav-link p-0 text-white">Mods</a></li>
+                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/modpacks"} className="nav-link p-0 text-white">Modpacks</a></li>
+                        <li className="nav-item mb-2"><a href={process.env.API_URL + "/textures"} className="nav-link p-0 text-white">Textures</a></li>
                       </ul>
                     </div>
 
                     <div className="col mb-3">
                       <h5>Resources</h5>
                       <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary disabled">Blog</a></li>
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary disabled">Docs</a></li>
-                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-body-secondary disabled">Status</a></li>
-                        <li className="nav-item mb-2"><a href="https://github.com/Roxxedo/modustry" className="nav-link p-0 text-body-secondary">Github</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white disabled">Blog</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white disabled">Docs</a></li>
+                        <li className="nav-item mb-2"><a href="#" className="nav-link p-0 text-white disabled">Status</a></li>
+                        <li className="nav-item mb-2"><a href="https://github.com/Roxxedo/modustry" className="nav-link p-0 text-white">Github</a></li>
                       </ul>
                     </div>
 
                     <div className="col mb-3">
                       <h5>Mindustry</h5>
                       <ul className="nav flex-column">
-                        <li className="nav-item mb-2"><a href="https://mindustrygame.github.io" className="nav-link p-0 text-body-secondary">Website</a></li>
-                        <li className="nav-item mb-2"><a href="https://discord.gg/mindustry" className="nav-link p-0 text-body-secondary">Discord</a></li>
-                        <li className="nav-item mb-2"><a href="https://github.com/Anuken/Mindustry" className="nav-link p-0 text-body-secondary">Github</a></li>
+                        <li className="nav-item mb-2"><a href="https://mindustrygame.github.io" className="nav-link p-0 text-white">Website</a></li>
+                        <li className="nav-item mb-2"><a href="https://discord.gg/mindustry" className="nav-link p-0 text-white">Discord</a></li>
+                        <li className="nav-item mb-2"><a href="https://github.com/Anuken/Mindustry" className="nav-link p-0 text-white">Github</a></li>
                       </ul>
                     </div>
                   </footer>
--- src/components/ListElement.tsx
+++ src/components/ListElement.tsx
@@ -5,8 +5,8 @@ import { Img } from 'react-image'
 export default function ListElement({ value }: GenericInputPropsData) {
     return (
         <>
-            <Link href={'/mod/' + value.repo.split('/')[1]} className="list-group-item list-group-item-action d-flex gap-3 my-1 rounded-4 text-white shadow" style={{background: "#26292f"}} aria-current="true" key={"mod-" + value.repo.split('/')[1]}>
-              <Img src={[("https://raw.githubusercontent.com/" + value.repo + "/master/icon.png"), ("https://raw.githubusercontent.com/" + value.repo + "/master/assets/icon.png"), "https://raw.githubusercontent.com/Anuken/MindustryJavaModTemplate/master/assets/sprites/frog.png"]} alt="Mod Icon" width="96" height="96" className="rounded-3 flex-shrink-0" />
+            <Link href={'/mod/' + value.repo.split('/')[1]} className="list-group-item list-group-item-action d-flex gap-3 my-1 rounded-4 text-white shadow border-0" style={{background: "#26292f"}} aria-current="true" key={"mod-" + value.repo.split('/')[1]}>
+              <Img src={[("https://raw.githubusercontent.com/" + value.repo + "/master/icon.png"), ("https://raw.githubusercontent.com/" + value.repo + "/master/assets/icon.png"), "/modustry.jpg"]} alt="Mod Icon" width="96" height="96" className="rounded-3 flex-shrink-0" />
               <div className="d-flex gap-2 w-100 justify-content-between">
                 <div>
                     <h3 className="mb-1" style={{display: "inline-block"}}>{value.name}</h3>
