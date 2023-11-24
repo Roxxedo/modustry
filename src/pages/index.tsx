03b79032474b7024e68b38d2cf1647f8fd73f4bc
Roxxedo <75433591+Roxxedo@users.noreply.github.com>
2023-11-24 16:34:45 -0300

    "Discover Mods" button color fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/Navbar'
 export default function Home() {
     return (
         <>
-            <Head title='Modustry' description='' />
+            <Head title='Modustry' description='The place for Mindustry Mods. Discover, play, and share Mindustry content through our open-source platform built for the community.' />
             <main className='main text-white shadow'>
                 <div className="container">
                     <Navbar />
@@ -12,7 +12,7 @@ export default function Home() {
                         <h1>The place for Mindustry <br/> Mods</h1>
                         <h2 className='center mb-4'> Discover, play, and share Mindustry content through our open-
                         <br />source platform built for the community. </h2>
-                        <a className='btn c-e5b276' href='/mods/'>Discover Mods</a>
+                        <a className='btn dmodsb' href='/mods/'>Discover Mods</a>
                     </section>
                 </div>
             </main>
