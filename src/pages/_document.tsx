c07ad9c25acac2b51a887b34c266a8a6320d5549
Roxxedo <75433591+Roxxedo@users.noreply.github.com>
2024-03-27 19:19:16 -0300

    Add SpeedInsights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import Footer from '@/components/Footer'
 import Navbar from '@/components/Navbar'
 import { Html, Head, Main, NextScript } from 'next/document'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
     return (
@@ -10,6 +11,7 @@ export default function Document() {
                 <Main />
                 <NextScript />
                 <Footer />
+                <SpeedInsights />
             </body>
         </Html>
     )
